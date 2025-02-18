4d2e306f5ccb4132265303aa65985a48926caba9
prathmesh2102003 <prathmeshamale646@gmail.com>
2025-02-18 11:30:01 +0000

    updated code

--- script.js
+++ script.js
@@ -1,3 +1,3 @@
-document.getElementById('action-btn').addEventListener('click', function() {
-    document.getElementById('message').textContent = "You clicked the button!";
+document.getElementById("clickMe").addEventListener("click", function () {
+    alert("Welcome to Ansible!");
 });
